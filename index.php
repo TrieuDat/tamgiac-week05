3980a56ce591c9052fa4af8c58c4ae62f69239d4
TrieuDat <44358856+TrieuDat@users.noreply.github.com>
2020-06-25 17:38:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+    
+    <h1>Kiểm tra loại tam giác</h1>
+    <form action="loaiTamGiac.php" method="get">
+        <label>a</label><br/>
+        <input type="text" name="a"><br/>
+        <label>b</label><br/>
+        <input type="text" name="b"><br/>
+        <label>c</label><br/>
+        <input type="text" name="c"><br/>
+        <br>
+        <input type="submit" name="dapan" value="Kết quả">
+        
+    </form>
+    <h1>Tính ngày kế tiếp</h1>
+    <form action="ngayKeTiep.php" method="get">
+        <label>Nhập ngày</label><br/>
+        <input type="text" name="day"><br/>
+        <label>Nhập tháng</label><br/>
+        <input type="text" name="month"><br/>
+        <label>Nhập năm</label><br/>
+        <input type="text" name="year"><br/>
+        <br>
+        <input type="submit" name="Caculate2" value="Kết quả" >
+        
+    </form>
+</body>
+</html>
+
 <?php
 function checkValidity($a, $b, $c) 
 { 
